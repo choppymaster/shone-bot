2ccabb7129dea696940aa44f438b5965696c4bb3
SS <74042715+choppymaster@users.noreply.github.com>
2021-01-22 13:42:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const { Client, MessageEmbed } = require('discord.js')
+const { MessageEmbed } = require('discord.js')
 const config = require('./config.json')
 
 client.on ('ready', message => { 
