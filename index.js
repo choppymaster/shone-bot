93c95e635b4a2ac1e3df4495d108614adba39d41
repl.it user <replituser@example.com>
2021-01-31 03:44:06 +0000

    lol

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const client = new Discord.Client()
 const { MessageEmbed } = require('discord.js')
 const db = require('quick.db')
 const fs = require('fs')
-const env = require('dotenv')
 
 client.commands = new Discord.Collection()
 
