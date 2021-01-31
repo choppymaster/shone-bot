1242ef32b4fd80933a0e0d9fe3acc00e02999317
SS <shonesaju8@gmail.com>
2021-01-31 07:55:40 +0000

    Okay

--- index.js
+++ index.js
@@ -69,8 +69,9 @@ client.on("message", async message => {
     if (message.author.bot) return;
     if (message.content.indexOf('!') === 0) {
         var text = message.content.substring(1);
-    chatbot.getReply(`${text}`, 'automatic').then(r => message.channel.send(`\`${message.author.username}\` ${r}`))
-    logger.log('info', `${message.author.tag} (${message.author.id}) talked to the bot by excecuting \`${text}\`.`)
+   const rep = chatbot.getReply(`${text}`, 'automatic')
+   message.channel.send(`\`${message.author.username}\` ${rep}`))
+    logger.log('info', `${message.author.tag} (${message.author.id}) talked to the bot by excecuting \`${text}\`. I replyed \`${rep}\` .`)
     }
 })
 
