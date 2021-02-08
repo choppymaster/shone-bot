f9a52b2f4f5ac8e1a6e0dd67d8a80919237d7cab
SS <shonesaju8@gmail.com>
2021-02-08 06:46:07 +0000

    okay

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const { MessageEmbed } = require('discord.js')
 const fs = require('fs')
 
 // Command handling 
