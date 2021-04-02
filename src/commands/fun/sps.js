664b169b6e8d23f08accf50c3004996c90052caa
SS <>
2021-04-02 09:23:31 +0000

    Updated .sps command to match the actual play

--- src/commands/fun/sps.js
+++ src/commands/fun/sps.js
@@ -3,7 +3,20 @@ module.exports.run = (client, message, args) => {
 	
 	const option = options[Math.floor(Math.random() * options.length)];
 	
-	message.channel.send(`You got ${option}`);
+	const input = args[0].toLowerCase();
+	if (!input) return message.channel.send("You didn't said an option!")
+	
+  let winner = null;
+  
+  if (input === "stone" && option === "scissors" || input === "paper" && option === "stone" || input === "scissors" && option === "paper") {
+    winner = `${message.author} wins!`;
+  } else if (input === option) {
+    winner = "No one wins."
+  } else {
+    winner = "I win!"
+  }
+  
+	message.channel.send(`You selected ${input}, I selected ${option}. ${winner}`);
 };
 
 module.exports.config = {
