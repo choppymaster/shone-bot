2eeaae0d68d459ffbde296be369e55c29b14fe70
SS <74042715+choppymaster@users.noreply.github.com>
2021-12-03 05:54:35 +0000

    Moderation: Add createrole and deleterole commands

---
+++ src/commands/Moderation/createrole.js
@@ -0,0 +1,30 @@
+module.exports = {
+  run: async (client, message, args) => {
+    const name = args.join(" ");
+
+    await message.guild.roles.create({ name: name });
+    message.channel.send(`Role ${name} created.`);
+  },
+  slashCommand: {
+    options: [
+      {
+        name: "name",
+        description: "The name of the role to create",
+        type: "STRING",
+        required: true
+      }
+    ]
+  },
+  execute: async (client, interaction, guild) => {
+    const name = interaction.options.getString("name");
+
+    await guild.roles.create({ name: name });
+    interaction.reply(`Role ${name} created.`);
+  },
+  config: {
+    name: "createrole",
+    description: "creates a role.",
+    guildOnly: true,
+    permissions: ["SEND_MESSAGES", "MANAGE_ROLES"]
+  }
+};
