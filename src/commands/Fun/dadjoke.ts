bc783ef7e2a96a2a1ed09633ca3246b040a6b335
SS <74042715+choppymaster@users.noreply.github.com>
2021-12-13 13:34:53 +0000

    Rewrite advice, joke and meme commands

--- src/commands/Fun/dadjoke.ts
+++ src/commands/Fun/dadjoke.ts
@@ -3,32 +3,33 @@ const axios = require("axios").default;
 
 export const Command = {
   run: async (client, message, args) => {
-    const advice = await axios.get("https://api.adviceslip.com/advice", {
+    const joke = await axios.get("https://icanhazdadjoke.com/", {
       headers: {
+        Accept: "text/plain",
         "User-Agent": `axios ${axios.VERSION}`
       }
     }).then(json => json.data);
     const embed = new MessageEmbed()
-      .setTitle(advice.slip.advice)
+      .setTitle(joke)
       .setColor("RANDOM");
-
     message.channel.send({ embeds: [embed] });
   },
   execute: async (client, interaction, guild) => {
-    const advice = await axios.get("https://api.adviceslip.com/advice", {
+    const joke = await axios.get("https://icanhazdadjoke.com/", {
       headers: {
+        Accept: "text/plain",
         "User-Agent": `axios ${axios.VERSION}`
       }
     }).then(json => json.data);
     const embed = new MessageEmbed()
-      .setTitle(advice.slip.advice)
+      .setTitle(joke)
       .setColor("RANDOM");
-
     interaction.reply({ embeds: [embed] });
   },
+
   config: {
-    name: "advice",
-    description: "Gives you a random advice.",
+    name: "dadjoke",
+    description: "Gives you a random dad joke.",
     permissions: ["SEND_MESSAGES"]
   }
 };
--- src/commands/Fun/meme.ts
+++ src/commands/Fun/meme.ts
@@ -1,9 +1,38 @@
-import srod = require("something-random-on-discord");
+import { MessageEmbed } from "discord.js";
+const axios = require("axios").default;
 
 export const Command = {
   run: async (client, message, args) => {
-    const meme = await srod.Random.getMeme();
-    message.channel.send({ embeds: [meme] });
+    const subreddits = ["memes", "me_irl", "MemeEconomy", "ComedyCemetery", "dankmemes", "comedyheaven", "Animemes"];
+    const subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
+
+    const meme = await axios.get(`https://www.reddit.com/r/${subreddit}/random/.json`, {
+      headers: {
+        "User-Agent": `axios ${axios.VERSION}`
+      }
+    }).then(data => data.data[0].data.children[0].data);
+    const embed = new MessageEmbed()
+      .setTitle(meme.title)
+      .setColor("RANDOM")
+      .setImage(meme.url);
+
+    message.channel.send({ embeds: [embed] });
+  },
+  execute: async (client, interaction, guild) => {
+    const subreddits = ["memes", "me_irl", "MemeEconomy", "ComedyCemetery", "dankmemes", "comedyheaven", "Animemes"];
+    const subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
+
+    const meme = await axios.get(`https://www.reddit.com/r/${subreddit}/random/.json`, {
+      headers: {
+        "User-Agent": `axios ${axios.VERSION}`
+      }
+    }).then(data => data.data[0].data.children[0].data);
+    const embed = new MessageEmbed()
+      .setTitle(meme.title)
+      .setColor("RANDOM")
+      .setImage(meme.url);
+
+    interaction.reply({ embeds: [embed] });
   },
 
   config: {
