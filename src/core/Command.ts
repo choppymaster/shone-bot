7dde83f049cec3b984926a1410b1cedb14b08825
SS <74042715+choppymaster@users.noreply.github.com>
2021-12-21 11:06:48 +0000

    K

--- src/core/Command.ts
+++ src/core/Command.ts
@@ -1,6 +1,8 @@
+import { ISlashOption } from "./interfaces";
+
 export default abstract class Command {
-  public name: string
-  public decsription: string
+  public name!: string
+  public decsription!: string
   public aliases!: string[]
   public cooldown!: number
   public guildOnly!: boolean
